9ac9a31387d0feaf89093ad1a8141f7d04284832
hong le <vuanhde@yahoo.de>
2024-10-16 12:58:19 +0200

    3.commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import './style.scss';
+import './app.scss';
 import Icon from './Icon.jsx';
 
-
 const App = () => (
-    <div>
-        <Icon />
-        <h1>Hello World!</h1>
+    <div className="container">
+        <div className="header">
+            <h1>Hello World!</h1>
+        </div>
+        <div className="react-logo">
+            <Icon />
+        </div>
     </div>
 )
 
--- src/Icon.jsx
+++ src/Icon.jsx
@@ -1,7 +1,8 @@
-import reactIcon from './react.svg';
+import reactIcon from './Assets/Images/react.svg';
 
 const Icon = () => (
-    <img src={reactIcon} alt="React Logo" width={64} height={64} />
+    // <img src={reactIcon} alt="React Logo" width={64} height={64} />
+    <img src={reactIcon} alt="React Logo" />
 );
 
 export default Icon;
